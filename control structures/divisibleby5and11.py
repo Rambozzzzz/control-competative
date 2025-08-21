3d0f0a67ad054f0d59626893f04beb649bb59cad
Rambozzzzz <anuj.rajput_bca24@gla.ac.in>
2025-08-21 08:20:06 +0530

    Update divisibleby5and11.py

--- control structures/divisibleby5and11.py
+++ control structures/divisibleby5and11.py
@@ -1,11 +1,10 @@
-# Take an integer A as input. You have to tell whether A is divisible by both 5 and 11 or not.#
-
 A = int(input("Enter an integer: "))
 
 if A % 5 == 0 and A % 11 == 0:
     print("YES")
 else:
     print("NO")
+
 
 
 
