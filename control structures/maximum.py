fefd00f446f9dc7e9c06ff9de7375d3b0eec1337
Rambozzzzz <anuj.rajput_bca24@gla.ac.in>
2025-08-21 08:20:55 +0530

    Update maximum.py

--- control structures/maximum.py
+++ control structures/maximum.py
@@ -1,8 +1,7 @@
-# Read three integers and print their maximum. @
-
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 print("Maximum number is:", max(a, b, c))
+
 
 
