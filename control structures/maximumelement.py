7835c3d7a76a6718e8119c6862f353aeb4340394
Rambozzzzz <anuj.rajput_bca24@gla.ac.in>
2025-08-21 08:21:25 +0530

    Update maximumelement.py

--- control structures/maximumelement.py
+++ control structures/maximumelement.py
@@ -1,7 +1,5 @@
-#Write a program to input two numbers(A & B) from the user and print the maximum 
-#element among A & B. 
-
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
+
 
 print("Maximum number is:", max(a, b))
